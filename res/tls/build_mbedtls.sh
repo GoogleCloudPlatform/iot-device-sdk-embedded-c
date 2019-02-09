a3be469b8fd4a709afa51f6e0de116e310c6070a
David Della Bitta <ddellabitta@gmail.com>
2019-02-09 16:27:22 -0500

    header / copyright update.

--- res/tls/build_mbedtls.sh
+++ res/tls/build_mbedtls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2018 Google LLC
 #
-# This is part of the Google Cloud IoT Edge Embedded C Client,
+# This is part of the Google Cloud IoT Device SDK for Embedded C,
 # it is licensed under the BSD 3-Clause license; you may not use this file
 # except in compliance with the License.
 #
