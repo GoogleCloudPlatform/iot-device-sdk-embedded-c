d62e710c2d4c321a82d1dc67e394802e616635e6
Frédéric Rechtenstein <frec@google.com>
2019-02-19 11:51:03 +0900

    reformat Python scripts to use PEP8 style

--- tools/format_code.py
+++ tools/format_code.py
@@ -23,43 +23,48 @@ import shlex
 
 CLANG_FORMAT = "clang-format -style=Google"
 
-def clangFormatFile( filename ):
-    args = shlex.split( CLANG_FORMAT )
-    args += [ "-i", filename ]
 
-    print( args )
+def clangFormatFile(filename):
+    args = shlex.split(CLANG_FORMAT)
+    args += ["-i", filename]
 
-    p = subprocess.Popen( args )
+    print(args)
 
-def findFiles( startDir, fileExt, recLevel, currLevel = 0 ):
-    contents    = os.listdir( startDir )
+    p = subprocess.Popen(args)
+
+
+def findFiles(startDir, fileExt, recLevel, currLevel=0):
+    contents = os.listdir(startDir)
 
     with open(".clang-format-ignore") as f:
         files_to_ignore = [x.strip('\n') for x in f.readlines()]
 
-    files   = [ x for x in contents if os.path.isfile( os.path.join( startDir, x ) ) and x.endswith( fileExt ) and x not in files_to_ignore ]
-    dirs    = [ x for x in contents if os.path.isdir( os.path.join( startDir, x ) ) and x[ 0 ] != '.' and x not in files_to_ignore ]
+    files = [x for x in contents if os.path.isfile(os.path.join(
+        startDir, x)) and x.endswith(fileExt) and x not in files_to_ignore]
+    dirs = [x for x in contents if os.path.isdir(os.path.join(
+        startDir, x)) and x[0] != '.' and x not in files_to_ignore]
 
     for f in files:
-        filename = os.path.join( startDir, f )
-        clangFormatFile( filename )
+        filename = os.path.join(startDir, f)
+        clangFormatFile(filename)
 
-    if recLevel == 0 or ( recLevel > 0 and currLevel < recLevel ):
+    if recLevel == 0 or (recLevel > 0 and currLevel < recLevel):
         for d in dirs:
-            findFiles( os.path.join( startDir, d ), fileExt, recLevel,
-                    currLevel + 1 )
+            findFiles(os.path.join(startDir, d), fileExt, recLevel,
+                      currLevel + 1)
+
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser( description='Source code formatter' )
-    parser.add_argument( '-r', dest='recursive', type=int, default=100,
-                       help='recursive mode, default 1, set 0 if you want to enable unlimited recursion')
+    parser = argparse.ArgumentParser(description='Source code formatter')
+    parser.add_argument('-r', dest='recursive', type=int, default=100,
+                        help='recursive mode, default 1, set 0 if you want to enable unlimited recursion')
 
-    args        = parser.parse_args()
-    recursive   = args.recursive
+    args = parser.parse_args()
+    recursive = args.recursive
 
-    directories = [ '../src/', '../include/', '../examples/' ];
+    directories = ['../src/', '../include/', '../examples/']
 
     for dir in directories:
-        findFiles( dir, ".h", recursive )
-        findFiles( dir, ".c", recursive )
+        findFiles(dir, ".h", recursive)
+        findFiles(dir, ".c", recursive)
