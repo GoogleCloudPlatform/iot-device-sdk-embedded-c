11e8862dacd855acfb517a6edc83cb21049597d9
Sungju Doh <sungju@google.com>
2019-03-11 12:24:01 +0900

    Refactoring

--- src/libiotc/iotc_roughtime_client.cc
+++ src/libiotc/iotc_roughtime_client.cc
@@ -51,62 +51,66 @@ iotc_roughtime_create_socket(int* out_socket, const char* server_address) {
   }
   std::string host(address.substr(0, kColonOffset));
   const std::string kPortStr(address.substr(kColonOffset + 1));
-  int port = std::stoi(kPortStr.c_str());
+  const int port = std::stoi(kPortStr.c_str());
 
   if (IOTC_BSP_IO_NET_STATE_OK !=
       iotc_bsp_io_net_socket_connect(
           reinterpret_cast<iotc_bsp_socket_t*>(out_socket), host.c_str(), port,
           SOCKET_DGRAM)) {
-    iotc_debug_logger("Connect to the socket");
+    iotc_debug_logger("ERROR: Connect to the socket");
+    return IOTC_ROUGHTIME_ERROR;
   }
 
   if (IOTC_BSP_IO_NET_STATE_OK !=
       iotc_bsp_io_net_connection_check(*out_socket, host.c_str(), port)) {
-    iotc_debug_logger("Check the connection");
+    iotc_debug_logger("ERROR: Check the connection");
+    return IOTC_ROUGHTIME_ERROR;
   }
 
   return IOTC_ROUGHTIME_OK;
 }
 
 iotc_roughtime_state_t
-iotc_roughtime_receive_time(int socket, const char* name,
-                            const char* public_key,
+iotc_roughtime_receive_time(int socket, const char* public_key,
                             iotc_roughtime_timedata_t* time_data) {
-
-  std::string server_name(name);
   std::string server_public_key(public_key);
+  // TODO(b/127770330)
+  // uint8_t nonce[roughtime::kNonceLength] = {0};
   uint8_t nonce[roughtime::kNonceLength];
-  size_t socket_evts_size = 1;
-  iotc_bsp_io_net_state_t state;
-  iotc_bsp_socket_events_t socket_evts[socket_evts_size];
+  bzero(nonce, roughtime::kNonceLength);
+  iotc_bsp_io_net_state_t state = IOTC_BSP_IO_NET_STATE_OK;
+  iotc_bsp_socket_events_t socket_event;
 
   for (size_t i = 0; i < roughtime::kNonceLength; i++) {
     nonce[i] = (uint8_t)iotc_bsp_rng_get() % 256;
   }
   const std::string kRequest = roughtime::CreateRequest(nonce);
   if (kRequest.empty()) {
-    iotc_debug_logger("Roughtime create reaquest "
-                      "failed");
+    iotc_debug_logger("ERROR: Roughtime create reaquest");
+    iotc_bsp_io_net_close_socket(reinterpret_cast<iotc_bsp_socket_t*>(&socket));
     return IOTC_ROUGHTIME_ERROR;
   }
 
   /* Write to the socket */
-  memset(socket_evts, 0, sizeof(iotc_bsp_socket_events_t) * socket_evts_size);
-  socket_evts[0].iotc_socket = socket;
-  socket_evts[0].in_socket_want_write = 1;
+  memset(&socket_event, 0, sizeof(iotc_bsp_socket_events_t));
+  socket_event.iotc_socket = socket;
+  socket_event.in_socket_want_write = 1;
   bool ready_to_write = false;
   while (!ready_to_write) {
-    state =
-        iotc_bsp_io_net_select(socket_evts, socket_evts_size, kTimeoutSeconds);
+    state = iotc_bsp_io_net_select(&socket_event, 1, kTimeoutSeconds);
     switch (state) {
     case IOTC_BSP_IO_NET_STATE_OK:
-      if (socket_evts[0].out_socket_can_write == 1) {
+      if (socket_event.out_socket_can_write == 1) {
         ready_to_write = true;
       }
       break;
     case IOTC_BSP_IO_NET_STATE_TIMEOUT:
+      iotc_bsp_io_net_close_socket(
+          reinterpret_cast<iotc_bsp_socket_t*>(&socket));
       return IOTC_ROUGHTIME_TIMEOUT_ERROR;
     case IOTC_BSP_IO_NET_STATE_ERROR:
+      iotc_bsp_io_net_close_socket(
+          reinterpret_cast<iotc_bsp_socket_t*>(&socket));
       return IOTC_ROUGHTIME_NETWORK_ERROR;
     default:
       break;
@@ -118,7 +122,7 @@ iotc_roughtime_receive_time(int socket, const char* name,
       socket, &bytes_written, reinterpret_cast<const uint8_t*>(kRequest.data()),
       kRequest.size());
   if (IOTC_BSP_IO_NET_STATE_ERROR == state) {
-    iotc_debug_logger("Write to the socket");
+    iotc_debug_logger("ERROR: Write to the socket");
     iotc_bsp_io_net_close_socket(reinterpret_cast<iotc_bsp_socket_t*>(&socket));
     return IOTC_ROUGHTIME_NETWORK_ERROR;
   }
@@ -126,28 +130,31 @@ iotc_roughtime_receive_time(int socket, const char* name,
 
   if (bytes_written < 0 ||
       static_cast<size_t>(bytes_written) != kRequest.size()) {
-    iotc_debug_logger("Write to the socket");
-    close(socket);
+    iotc_debug_logger("ERROR: Write to the socket");
+    iotc_bsp_io_net_close_socket(reinterpret_cast<iotc_bsp_socket_t*>(&socket));
     return IOTC_ROUGHTIME_NETWORK_ERROR;
   }
 
   /* Read from the socket */
-  memset(socket_evts, 0, sizeof(iotc_bsp_socket_events_t) * socket_evts_size);
-  socket_evts[0].iotc_socket = socket;
-  socket_evts[0].in_socket_want_read = 1;
+  memset(&socket_event, 0, sizeof(iotc_bsp_socket_events_t));
+  socket_event.iotc_socket = socket;
+  socket_event.in_socket_want_read = 1;
   bool ready_to_read = false;
   while (!ready_to_read) {
-    state =
-        iotc_bsp_io_net_select(socket_evts, socket_evts_size, kTimeoutSeconds);
+    state = iotc_bsp_io_net_select(&socket_event, 1, kTimeoutSeconds);
     switch (state) {
     case IOTC_BSP_IO_NET_STATE_OK:
-      if (socket_evts[0].out_socket_can_read == 1) {
+      if (socket_event.out_socket_can_read == 1) {
         ready_to_read = true;
       }
       break;
     case IOTC_BSP_IO_NET_STATE_TIMEOUT:
+      iotc_bsp_io_net_close_socket(
+          reinterpret_cast<iotc_bsp_socket_t*>(&socket));
       return IOTC_ROUGHTIME_TIMEOUT_ERROR;
     case IOTC_BSP_IO_NET_STATE_ERROR:
+      iotc_bsp_io_net_close_socket(
+          reinterpret_cast<iotc_bsp_socket_t*>(&socket));
       return IOTC_ROUGHTIME_NETWORK_ERROR;
     default:
       break;
@@ -158,7 +165,7 @@ iotc_roughtime_receive_time(int socket, const char* name,
   int buf_len;
   state = iotc_bsp_io_net_read(socket, &buf_len, recv_buf, sizeof(recv_buf));
   if (IOTC_BSP_IO_NET_STATE_ERROR == state) {
-    iotc_debug_logger("Read from the socket");
+    iotc_debug_logger("ERROR: Read from the socket");
     iotc_bsp_io_net_close_socket(reinterpret_cast<iotc_bsp_socket_t*>(&socket));
     return IOTC_ROUGHTIME_NETWORK_ERROR;
   }
@@ -168,12 +175,12 @@ iotc_roughtime_receive_time(int socket, const char* name,
 
   if (buf_len == -1) {
     if (errno == EINTR) {
-      iotc_debug_format(stderr, "No response from %s with %d seconds.\n",
-                        server_name.c_str(), kTimeoutSeconds);
+      iotc_debug_format(stderr, "No response whithin %d seconds.\n",
+                        kTimeoutSeconds);
       return IOTC_ROUGHTIME_TIMEOUT_ERROR;
     }
 
-    iotc_debug_logger("Read from the socket");
+    iotc_debug_logger("ERROR: Read from the socket");
     return IOTC_ROUGHTIME_NETWORK_ERROR;
   }
 
@@ -182,8 +189,8 @@ iotc_roughtime_receive_time(int socket, const char* name,
           &(time_data->timestamp), &(time_data->radius), &error,
           reinterpret_cast<const uint8_t*>(server_public_key.data()), recv_buf,
           buf_len, nonce)) {
-    iotc_debug_format(stderr, "Response from %s failed verification: %s",
-                      server_name.c_str(), error.c_str());
+    iotc_debug_format(stderr, "ERROR: Response failed verification: %s",
+                      error.c_str());
     return IOTC_ROUGHTIME_RECEIVE_TIME_ERROR;
   }
 
@@ -191,6 +198,7 @@ iotc_roughtime_receive_time(int socket, const char* name,
   // add half the round-trip time to the server's timestamp to produce our
   // estimate of the current time.
   time_data->reply_time = (kEndUs - kStartUs);
+  // Convert precision of timestamp from microsecond to millisecond.
   time_data->timestamp /= 1000;
   time_data->timestamp += (time_data->reply_time) / 2;
 
@@ -198,7 +206,7 @@ iotc_roughtime_receive_time(int socket, const char* name,
 }
 
 iotc_roughtime_state_t
-iotc_roughtime_getcurrenttime(const char* name, const char* public_key,
+iotc_roughtime_getcurrenttime(const char* public_key,
                               const char* server_address,
                               iotc_roughtime_timedata_t* time_data) {
   int socket;
@@ -206,12 +214,12 @@ iotc_roughtime_getcurrenttime(const char* name, const char* public_key,
   for (int i = 0; i < kRetrialTimes; i++) {
     if ((state = iotc_roughtime_create_socket(&socket, server_address)) !=
         IOTC_ROUGHTIME_OK) {
-      iotc_debug_logger("Failed on connecting socket.");
+      iotc_debug_logger("ERROR: Create socket");
       continue;
     }
-    if ((state = iotc_roughtime_receive_time(socket, name, public_key,
-                                             time_data)) != IOTC_ROUGHTIME_OK) {
-      iotc_debug_logger("Failed on receving time is failed.");
+    if ((state = iotc_roughtime_receive_time(socket, public_key, time_data)) !=
+        IOTC_ROUGHTIME_OK) {
+      iotc_debug_logger("ERROR: Receive time");
       continue;
     }
     return IOTC_ROUGHTIME_OK;
