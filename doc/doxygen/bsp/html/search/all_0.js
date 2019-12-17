2b517e57ca390ede659734ec2a67019be8cf61e0
Cyrus Maden <cmaden@google.com>
2019-12-17 17:05:25 -0500

    Address comments on other API files

---
+++ doc/doxygen/bsp/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ca_5fcert_5fpem_5fbuf',['ca_cert_pem_buf',['../d9/d57/structiotc__bsp__tls__init__params__s.html#a3c803f4af03b770bd54aa0bd245688a2',1,'iotc_bsp_tls_init_params_s']]],
+  ['ca_5fcert_5fpem_5fbuf_5flength',['ca_cert_pem_buf_length',['../d9/d57/structiotc__bsp__tls__init__params__s.html#a5a4d47a300299fae3a9f3d0ef6ff4729',1,'iotc_bsp_tls_init_params_s']]]
+];
