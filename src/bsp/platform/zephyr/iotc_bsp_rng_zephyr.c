e84b610e4f637bc26370d163ce731270dab1b418
Arpad Tigyi <atigyi@google.com>
2020-02-04 14:16:25 +0100

    Fix of first commit: Copyright 2019 -> Copyright 2019-2020

--- src/bsp/platform/zephyr/iotc_bsp_rng_zephyr.c
+++ src/bsp/platform/zephyr/iotc_bsp_rng_zephyr.c
@@ -1,4 +1,4 @@
-/* Copyright 2018-2020 Google LLC
+/* Copyright 2019-2020 Google LLC
  *
  * This is part of the Google Cloud IoT Device SDK for Embedded C.
  * It is licensed under the BSD 3-Clause license; you may not use this file
