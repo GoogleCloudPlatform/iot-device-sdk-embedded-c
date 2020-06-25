d0389c24465cd1da73d2789e9543db2bfd6cf412
Gal Zahavi <galz100@gmail.com>
2020-06-24 20:28:33 -0700

    Added espressif gateway example

---
+++ examples/espressif/esp32-gateway/Delegate-device/main/ble_mesh_demo_main.c
@@ -0,0 +1,312 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * Additional Copyright (c) 2018 Espressif Systems (Shanghai) PTE LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "esp_ble_mesh_defs.h"
+#include "esp_ble_mesh_common_api.h"
+#include "esp_ble_mesh_networking_api.h"
+#include "esp_ble_mesh_provisioning_api.h"
+#include "esp_ble_mesh_config_model_api.h"
+#include "esp_ble_mesh_local_data_operation_api.h"
+
+#include "ble_mesh_demo_init.h"
+
+#include "driver/gpio.h"
+
+#define CID_ESP     0x02E5
+#define CID_NVAL    0xFFFF
+
+#define BLINK_GPIO 22
+
+#define ESP_BLE_MESH_VND_MODEL_ID_CLIENT    0x0000
+#define ESP_BLE_MESH_VND_MODEL_ID_SERVER    0x0001
+
+#define ESP_BLE_MESH_VND_MODEL_OP_SEND      ESP_BLE_MESH_MODEL_OP_3(0x00, CID_ESP)
+#define ESP_BLE_MESH_VND_MODEL_OP_STATUS    ESP_BLE_MESH_MODEL_OP_3(0x01, CID_ESP)
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    uint8_t temprature_sens_read();
+
+#ifdef __cplusplus
+}
+#endif
+
+uint8_t temprature_sens_read();
+
+char* recievedCommand = "";
+char* delegateDevice = "device";
+
+static uint8_t dev_uuid[ESP_BLE_MESH_OCTET16_LEN] = { 0x32, 0x10 };
+
+static esp_ble_mesh_cfg_srv_t config_server = {
+    .relay = ESP_BLE_MESH_RELAY_DISABLED,
+    .beacon = ESP_BLE_MESH_BEACON_ENABLED,
+#if defined(CONFIG_BLE_MESH_FRIEND)
+    .friend_state = ESP_BLE_MESH_FRIEND_ENABLED,
+#else
+    .friend_state = ESP_BLE_MESH_FRIEND_NOT_SUPPORTED,
+#endif
+#if defined(CONFIG_BLE_MESH_GATT_PROXY_SERVER)
+    .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_ENABLED,
+#else
+    .gatt_proxy = ESP_BLE_MESH_GATT_PROXY_NOT_SUPPORTED,
+#endif
+    .default_ttl = 7,
+    /* 3 transmissions with 20ms interval */
+    .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+    .relay_retransmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+};
+
+static esp_ble_mesh_model_t root_models[] = {
+    ESP_BLE_MESH_MODEL_CFG_SRV(&config_server),
+};
+
+static esp_ble_mesh_model_op_t vnd_op[] = {
+    ESP_BLE_MESH_MODEL_OP(ESP_BLE_MESH_VND_MODEL_OP_SEND, 2),
+    ESP_BLE_MESH_MODEL_OP_END,
+};
+
+static esp_ble_mesh_model_t vnd_models[] = {
+    ESP_BLE_MESH_VENDOR_MODEL(CID_ESP, ESP_BLE_MESH_VND_MODEL_ID_SERVER,
+    vnd_op, NULL, NULL),
+};
+
+static esp_ble_mesh_elem_t elements[] = {
+    ESP_BLE_MESH_ELEMENT(0, root_models, vnd_models),
+};
+
+static esp_ble_mesh_comp_t composition = {
+    .cid = CID_ESP,
+    .elements = elements,
+    .element_count = ARRAY_SIZE(elements),
+};
+
+static esp_ble_mesh_prov_t provision = {
+    .uuid = dev_uuid,
+};
+
+static void prov_complete(uint16_t net_idx, uint16_t addr, uint8_t flags, uint32_t iv_index)
+{
+    ESP_LOGI(TAG, "net_idx 0x%03x, addr 0x%04x", net_idx, addr);
+    ESP_LOGI(TAG, "flags 0x%02x, iv_index 0x%08x", flags, iv_index);
+    gpio_set_level(BLINK_GPIO, 0);
+}
+
+static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
+                                             esp_ble_mesh_prov_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_BLE_MESH_PROV_REGISTER_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_PROV_REGISTER_COMP_EVT, err_code %d", param->prov_register_comp.err_code);
+        break;
+    case ESP_BLE_MESH_NODE_PROV_ENABLE_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_ENABLE_COMP_EVT, err_code %d", param->node_prov_enable_comp.err_code);
+        break;
+    case ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_OPEN_EVT, bearer %s",
+            param->node_prov_link_open.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+        break;
+    case ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_LINK_CLOSE_EVT, bearer %s",
+            param->node_prov_link_close.bearer == ESP_BLE_MESH_PROV_ADV ? "PB-ADV" : "PB-GATT");
+        break;
+    case ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_COMPLETE_EVT");
+        prov_complete(param->node_prov_complete.net_idx, param->node_prov_complete.addr,
+            param->node_prov_complete.flags, param->node_prov_complete.iv_index);
+        break;
+    case ESP_BLE_MESH_NODE_PROV_RESET_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_PROV_RESET_EVT");
+        break;
+    case ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT:
+        ESP_LOGI(TAG, "ESP_BLE_MESH_NODE_SET_UNPROV_DEV_NAME_COMP_EVT, err_code %d", param->node_set_unprov_dev_name_comp.err_code);
+        break;
+    default:
+        break;
+    }
+}
+
+static void example_ble_mesh_config_server_cb(esp_ble_mesh_cfg_server_cb_event_t event, esp_ble_mesh_cfg_server_cb_param_t *param)
+{
+    if (event == ESP_BLE_MESH_CFG_SERVER_STATE_CHANGE_EVT) {
+        switch (param->ctx.recv_op) {
+        case ESP_BLE_MESH_MODEL_OP_APP_KEY_ADD:
+            ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_APP_KEY_ADD");
+            ESP_LOGI(TAG, "net_idx 0x%04x, app_idx 0x%04x", param->value.state_change.appkey_add.net_idx, param->value.state_change.appkey_add.app_idx);
+            ESP_LOG_BUFFER_HEX("AppKey", param->value.state_change.appkey_add.app_key, 16);
+            break;
+        case ESP_BLE_MESH_MODEL_OP_MODEL_APP_BIND:
+            ESP_LOGI(TAG, "ESP_BLE_MESH_MODEL_OP_MODEL_APP_BIND");
+            ESP_LOGI(TAG, "elem_addr 0x%04x, app_idx 0x%04x, cid 0x%04x, mod_id 0x%04x", param->value.state_change.mod_app_bind.element_addr, param->value.state_change.mod_app_bind.app_idx, param->value.state_change.mod_app_bind.company_id, param->value.state_change.mod_app_bind.model_id);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+static void example_ble_mesh_custom_model_cb(esp_ble_mesh_model_cb_event_t event, esp_ble_mesh_model_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_BLE_MESH_MODEL_OPERATION_EVT:
+        if (param->model_operation.opcode == ESP_BLE_MESH_VND_MODEL_OP_SEND) {
+            uint8_t write_char_data[20];
+            char temp[50];
+
+            recievedCommand = (char *)param->model_operation.msg;
+            ESP_LOGI(TAG, "Recv 0x%06x, tid %s", param->model_operation.opcode, (char *)param->model_operation.msg);
+
+            char *device = strtok((char*)param->model_operation.msg,"/");
+            char* message = strtok(NULL,"/");
+
+            if (strcmp(device, delegateDevice) == 0){
+
+                if (strcmp(message, "Get Temp") == 0)
+                {
+                    sprintf(temp, "%f", ((temprature_sens_read() - 32) / 1.8));
+                    const char *msg1 = temp;
+                    size_t length = strlen(msg1) + 1;
+
+                    const char *beg = msg1;
+                    const char *end = msg1 + length;
+
+                    size_t i = 0;
+                    for (; beg != end; ++beg, ++i)
+                    {
+                        write_char_data[i] = (uint8_t)(*beg);
+                    }
+                }
+
+                if (strcmp(message, "Get State") == 0)
+                {
+                    const char *msg1 = "Device Connected";
+                    size_t length = strlen(msg1) + 1;
+
+                    const char *beg = msg1;
+                    const char *end = msg1 + length;
+
+                    size_t i = 0;
+                    for (; beg != end; ++beg, ++i)
+                    {
+                        write_char_data[i] = (uint8_t)(*beg);
+                    }
+                }
+
+                esp_err_t err = esp_ble_mesh_server_model_send_msg(&vnd_models[0], param->model_operation.ctx, ESP_BLE_MESH_VND_MODEL_OP_STATUS, sizeof(write_char_data), write_char_data);
+
+                if (err)
+                {
+                    ESP_LOGE(TAG, "Faild to send message 0x%06x", ESP_BLE_MESH_VND_MODEL_OP_STATUS);
+                }
+                else
+                {
+                    ESP_LOGW(TAG, "SENT");
+                    gpio_pad_select_gpio(BLINK_GPIO);
+                    /* Set the GPIO as a push/pull output */
+                    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        /* Blink off (output low) */
+                        gpio_set_level(BLINK_GPIO, 0);
+                        vTaskDelay(250 / portTICK_PERIOD_MS);
+                        gpio_set_level(BLINK_GPIO, 1);
+                        vTaskDelay(250 / portTICK_PERIOD_MS);
+                    }
+                }
+                ESP_LOGI(TAG, "Sent Response: %s \n",temp);
+            }
+        }
+        break;
+    case ESP_BLE_MESH_MODEL_SEND_COMP_EVT:
+        if (param->model_send_comp.err_code) {
+            ESP_LOGE(TAG, "Failed to send message 0x%06x", param->model_send_comp.opcode);
+            break;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            gpio_set_level(BLINK_GPIO, 0);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            gpio_set_level(BLINK_GPIO, 1);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+static esp_err_t ble_mesh_init(void)
+{
+    esp_err_t err;
+
+    esp_ble_mesh_register_prov_callback(example_ble_mesh_provisioning_cb);
+    esp_ble_mesh_register_config_server_callback(example_ble_mesh_config_server_cb);
+    esp_ble_mesh_register_custom_model_callback(example_ble_mesh_custom_model_cb);
+
+    err = esp_ble_mesh_init(&provision, &composition);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize mesh stack");
+        return err;
+    }
+
+    err = esp_ble_mesh_node_prov_enable(ESP_BLE_MESH_PROV_ADV | ESP_BLE_MESH_PROV_GATT);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to enable mesh node");
+        return err;
+    }
+
+    gpio_set_level(BLINK_GPIO, 1);
+
+    ESP_LOGI(TAG, "BLE Mesh Node initialized");
+
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    esp_err_t err;
+
+    ESP_LOGI(TAG, "Initializing...");
+
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_level(BLINK_GPIO, 0);
+
+    err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    gpio_set_level(BLINK_GPIO, 1);
+
+    err = bluetooth_init();
+    if (err) {
+        ESP_LOGE(TAG, "esp32_bluetooth_init failed (err %d)", err);
+        return;
+    }
+
+    ble_mesh_get_dev_uuid(dev_uuid);
+
+    /* Initialize the Bluetooth Mesh Subsystem */
+    err = ble_mesh_init();
+    if (err) {
+        ESP_LOGE(TAG, "Bluetooth mesh init failed (err %d)", err);
+    }
+}
---
+++ examples/espressif/esp32-gateway/Gateway-device/main/include.h
@@ -0,0 +1,192 @@
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "jsmn.h"
+#include <time.h>
+#include "lwip/apps/sntp.h"
+#include "driver/gpio.h"
+
+#include <esp32/rom/uart.h>
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include <iotc.h>
+#include <iotc_jwt.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sdkconfig.h>
+#include "board.h"
+/* BLE */
+#ifdef CONFIG_BT_BLUEDROID_ENABLED
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#endif
+
+#ifdef CONFIG_BT_NIMBLE_ENABLED
+#include "esp_nimble_hci.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "console/console.h"
+#endif
+
+#include "esp_ble_mesh_common_api.h"
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "esp_ble_mesh_provisioning_api.h"
+#include "esp_ble_mesh_networking_api.h"
+#include "esp_ble_mesh_config_model_api.h"
+
+#include "ble_mesh_demo_init.h"
+#include "driver/gpio.h"
+
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "iot_button.h"
+
+#define BUTTON_IO_NUM 0
+#define BUTTON_ACTIVE_LEVEL 0
+
+#define BLINK_GPIO 22
+
+#define CID_ESP 0x02E5
+#define CID_NVAL 0xFFFF
+
+#define PROV_OWN_ADDR 0x0001
+
+#define MSG_SEND_TTL 3
+#define MSG_SEND_REL false
+#define MSG_TIMEOUT 0
+#define MSG_ROLE ROLE_PROVISIONER
+
+#define COMP_DATA_PAGE_0 0x00
+
+#define APP_KEY_IDX 0x0000
+#define APP_KEY_OCTET 0x12
+
+#define COMP_DATA_1_OCTET(msg, offset) (msg[offset])
+#define COMP_DATA_2_OCTET(msg, offset) (msg[offset + 1] << 8 | msg[offset])
+
+#define ESP_BLE_MESH_VND_MODEL_ID_CLIENT 0x0000
+#define ESP_BLE_MESH_VND_MODEL_ID_SERVER 0x0001
+
+#define ESP_BLE_MESH_VND_MODEL_OP_SEND ESP_BLE_MESH_MODEL_OP_3(0x00, CID_ESP)
+#define ESP_BLE_MESH_VND_MODEL_OP_STATUS ESP_BLE_MESH_MODEL_OP_3(0x01, CID_ESP)
+
+static uint8_t dev_uuid[ESP_BLE_MESH_OCTET16_LEN];
+static uint16_t server_address = ESP_BLE_MESH_ADDR_UNASSIGNED;
+static uint16_t vnd_tid;
+static int64_t start_time;
+
+static struct esp_ble_mesh_key
+{
+    uint16_t net_idx;
+    uint16_t app_idx;
+    uint8_t app_key[ESP_BLE_MESH_OCTET16_LEN];
+} prov_key;
+
+static esp_ble_mesh_cfg_srv_t config_server = {
+    .beacon = ESP_BLE_MESH_BEACON_ENABLED,
+#if defined(CONFIG_BLE_MESH_FRIEND)
+    .friend_state = ESP_BLE_MESH_FRIEND_ENABLED,
+#else
+    .friend_state = ESP_BLE_MESH_FRIEND_NOT_SUPPORTED,
+#endif
+    .default_ttl = 7,
+    /* 3 transmissions with 20ms interval */
+    .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+    .relay_retransmit = ESP_BLE_MESH_TRANSMIT(2, 20),
+};
+
+static esp_ble_mesh_client_t config_client;
+
+static const esp_ble_mesh_client_op_pair_t vnd_op_pair[] = {
+    {ESP_BLE_MESH_VND_MODEL_OP_SEND, ESP_BLE_MESH_VND_MODEL_OP_STATUS},
+};
+
+static esp_ble_mesh_client_t vendor_client = {
+    .op_pair_size = ARRAY_SIZE(vnd_op_pair),
+    .op_pair = vnd_op_pair,
+};
+
+static esp_ble_mesh_model_op_t vnd_op[] = {
+    ESP_BLE_MESH_MODEL_OP(ESP_BLE_MESH_VND_MODEL_OP_STATUS, 2),
+    ESP_BLE_MESH_MODEL_OP_END,
+};
+
+static esp_ble_mesh_model_t root_models[] = {
+    ESP_BLE_MESH_MODEL_CFG_SRV(&config_server),
+    ESP_BLE_MESH_MODEL_CFG_CLI(&config_client),
+};
+
+static esp_ble_mesh_model_t vnd_models[] = {
+    ESP_BLE_MESH_VENDOR_MODEL(CID_ESP, ESP_BLE_MESH_VND_MODEL_ID_CLIENT, vnd_op, NULL, &vendor_client),
+};
+
+static esp_ble_mesh_elem_t elements[] = {
+    ESP_BLE_MESH_ELEMENT(0, root_models, vnd_models),
+};
+
+static esp_ble_mesh_comp_t composition = {
+    .cid = CID_ESP,
+    .elements = elements,
+    .element_count = ARRAY_SIZE(elements),
+};
+
+static esp_ble_mesh_prov_t provision = {
+    .prov_uuid = dev_uuid,
+    .prov_unicast_addr = PROV_OWN_ADDR,
+    .prov_start_address = 0x0005,
+};
+
+extern const uint8_t ec_pv_key_start[] asm("_binary_private_key_pem_start");
+extern const uint8_t ec_pv_key_end[] asm("_binary_private_key_pem_end");
+
+char *bleRecievedMessage = "";
+
+static char *delegateDevices[] = {"device", "device2"};
+static char *telemetryDevice = "";
+static char *delegate = "";
+char *command = "";
+char *device = "";
+int count = 0;
+
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+#define IOTC_UNUSED(x) (void)(x)
+
+#define DEVICE_PATH "projects/%s/locations/%s/registries/%s/devices/%s"
+#define SUBSCRIBE_TOPIC_COMMAND "/devices/%s/commands/#"
+#define SUBSCRIBE_TOPIC_CONFIG "/devices/%s/config"
+#define PUBLISH_TOPIC_EVENT "/devices/%s/events"
+#define PUBLISH_TOPIC_STATE "/devices/%s/state"
+#define PUBLISH_TOPIC_ATTACH "/devices/%s/attach"
+#define PUBLISH_TOPIC "/devices/%s/%s"
+#define BLE_MESSEGE "%s/%s"
+#define TEMPERATURE_DATA "{\"temp\" :\"%s\"}"
+#define MIN_TEMP 20
+#define OUTPUT_GPIO CONFIG_OUTPUT_GPIO
+
+char *subscribe_topic_command, *subscribe_topic_config, *publish_topic_attach, *cloudCommand;
+
+iotc_mqtt_qos_t iotc_example_qos = IOTC_MQTT_QOS_AT_LEAST_ONCE;
+static iotc_timed_task_handle_t delayed_publish_task =
+    IOTC_INVALID_TIMED_TASK_HANDLE;
+iotc_context_handle_t iotc_context = IOTC_INVALID_CONTEXT_HANDLE;
+
+bool passed = false;
