bf9fec0c7c7f7a6468fcd28f12b0a35ad806b088
Gus Class <gguuss@gmail.com>
2020-06-26 12:14:02 -0700

    Remove intermediate build files

--- examples/espressif/esp32-gateway/Gateway-device/build/button/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/button/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/button -lbutton
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += button
-COMPONENT_LDFRAGMENTS += 
-component-button-build: 
